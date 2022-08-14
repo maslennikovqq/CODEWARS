f05ba053ee784d86d138ebbe62895034cf1163be
maslennikovqq <maslennikovqq@gmail.com>
2022-08-14 20:51:12 +0300

    added 2 solutions

---
+++ Understanding closures - the basics.js
@@ -0,0 +1,11 @@
+function buildFun(n){
+    var res = []
+    for (var i = 0; i< n; i++){
+      (function(number){
+        res.push(function(){
+          return number;
+        })
+      })(i)
+    }
+    return res
+  }
